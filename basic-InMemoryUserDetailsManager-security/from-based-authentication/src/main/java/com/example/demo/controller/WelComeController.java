eafefeb40e3daba222e4d9535cf0ac7fd34d5b26
bhuduttsharma <76999431+bhuduttsharma@users.noreply.github.com>
2024-03-26 16:01:24 +0530

    test

---
+++ basic-InMemoryUserDetailsManager-security/from-based-authentication/src/main/java/com/example/demo/controller/WelComeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelComeController {
+
+	@GetMapping("/")
+    public String greeting() {
+        return "welcome";
+    }
+}
